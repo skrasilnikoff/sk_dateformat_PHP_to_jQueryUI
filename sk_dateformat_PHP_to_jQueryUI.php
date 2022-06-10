b6ae76c535d8655ac13904d8019d8ba1db2b0ad1
Serhii Krasylnykov <skrasilnikov.dev@gmail.com>
2022-06-10 10:28:56 +0300

    first commit

---
+++ sk_dateformat_PHP_to_jQueryUI.php
@@ -0,0 +1,68 @@
+<?php
+
+public static function dateformat_PHP_to_jQueryUI($php_format) {
+
+        $symbols_matching = [
+            // Day
+            'd' => 'dd',
+            'D' => 'D',
+            'j' => 'd',
+            'l' => 'DD',
+            'N' => '',
+            'S' => '',
+            'w' => '',
+            'z' => 'o',
+            // Week
+            'W' => '',
+            // Month
+            'F' => 'MM',
+            'm' => 'mm',
+            'M' => 'M',
+            'n' => 'm',
+            't' => '',
+            // Year
+            'L' => '',
+            'o' => '',
+            'Y' => 'yy',
+            'y' => 'y',
+            // Time
+            'a' => '',
+            'A' => '',
+            'B' => '',
+            'g' => '',
+            'G' => '',
+            'h' => '',
+            'H' => '',
+            'i' => '',
+            's' => '',
+            'u' => ''
+        ];
+        $jqueryui_format = "";
+
+        $escaping = false;
+        for ($i = 0; $i < strlen($php_format); $i++) {
+            $char = $php_format[$i];
+            if ($char === '\\') // PHP date format escaping character
+            {
+                $i++;
+                if ($escaping) {
+                    $jqueryui_format .= $php_format[$i];
+                } else {
+                    $jqueryui_format .= '\'' . $php_format[$i];
+                }
+                $escaping = true;
+            } else {
+                if ($escaping) {
+                    $jqueryui_format .= "'";
+                    $escaping = false;
+                }
+                if (isset($symbols_matching[$char])) {
+                    $jqueryui_format .= $symbols_matching[$char];
+                } else {
+                    $jqueryui_format .= $char;
+                }
+            }
+        }
+
+        return $jqueryui_format;
+    }
